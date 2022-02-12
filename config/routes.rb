8bb28cf6d02acb3a4f9e6e7c0e0cf0bc17d06086
Maximiliano Zavala <m.zavala22@tamu.edu>
2022-02-11 20:36:50 -0600

    Created page controller with views index(root route), about(/about), recruitment(/recruitment), and contact(/contact)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'page#index'
+
+  get '/index' => 'page#index'
+  get '/about' => 'page#about'
+  get '/contact' => 'page#contact'
+  get '/recruitment' => 'page#recruitment'
 
   resources :academic_tracker do 
   end
