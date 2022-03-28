d1f4d3ab2b7d619465ef762d0557869d59119bf4
annebell881 <aeb178@tamu.edu>
2022-03-28 17:48:47 -0500

    checking nav bar

--- spec/requests/user_flows_spec.rb
+++ spec/requests/user_flows_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "UserFlows", type: :request do
-  describe "GET /user_flows" do
-    it "works! (now write some real specs)" do
-      get user_flows_index_path
-      expect(response).to have_http_status(200)
+  describe "Navigation should be available" do
+    it "should show the navigation on the home page" do
+      visit root_path
+      page.should have_link('Home')
+      page.should have_link('About')
+      page.should have_link('Recruitment')
     end
   end
 end
