5af102104b2993d1f0bf8e1adfe5ef132d64ac71
Ajay <aj.verde79@gmail.com>
2022-03-30 08:51:10 -0500

    added routes for messages and support_logs

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :sponsors
   resources :users
   resources :sessions
+  resources: messages
+  resources: support_logs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/index' => 'page#index'
