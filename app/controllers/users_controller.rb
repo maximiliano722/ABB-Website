d33a77509933481dc736c83a21213e7baa51dabf
khaihuyennguyen90 <khaihuyennguyen90@tamu.edu>
2022-04-02 19:08:29 -0500

    comple user controllser -- without update ssl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        #format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
