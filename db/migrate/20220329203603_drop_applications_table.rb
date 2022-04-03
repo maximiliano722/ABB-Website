5223b215643bd40684b9643fce4d47e5fd5ac3d0
Ajay <aj.verde79@gmail.com>
2022-04-02 23:55:17 -0500

    added from originalAjay to ajay3

---
+++ db/migrate/20220329203603_drop_applications_table.rb
@@ -0,0 +1,9 @@
+class DropApplicationsTable < ActiveRecord::Migration[6.1]
+    def up
+      drop_table :applications
+    end
+  
+    def down
+      raise ActiveRecord::IrreversibleMigration 
+    end
+  end
---
+++ spec/views/support_logs/edit.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "support_logs/edit", type: :view do
+  before(:each) do
+    @support_log = assign(:support_log, SupportLog.create!(
+      event_id: 1,
+      sponsor_id: 1
+    ))
+  end
+
+  it "renders the edit support_log form" do
+    render
+
+    assert_select "form[action=?][method=?]", support_log_path(@support_log), "post" do
+
+      assert_select "input[name=?]", "support_log[event_id]"
+
+      assert_select "input[name=?]", "support_log[sponsor_id]"
+    end
+  end
+end
