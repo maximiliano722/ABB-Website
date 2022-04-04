149e704866151d4edc9b10af75d5f98160a9baf1
annebell881 <aeb178@tamu.edu>
2022-04-03 22:12:59 -0500

    testing simple path for home page nav bar

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "HomePages", type: :request do
   describe "able to access the about page from home" do
     it "get pages from nav bar" do
       get page_index_url
-      page.should have_link(page_about_url)
-      page.should have_link(page_contact_url)
-      page.should have_link(page_recruitment_url)
+      page.should have_link(href="/index")
+      page.should have_link(href="/about")
+      page.should have_link(href="/recruitment")
     end
   end
 end
