b898d3fd4940e2d156459eb0be94a034e1fb6180
annebell881 <aeb178@tamu.edu>
2022-04-06 17:13:20 -0500

    fixing gemfile bugs

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "HomePages", type: :request do
   describe "able to access the about page from home" do
     it "get pages from nav bar" do
-      get :index
+      get root_path
       page.should have_link(href="/index")
       page.should have_link(href="/about")
       page.should have_link(href="/recruitment")
