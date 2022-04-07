6204fb1ac64f1d74730723fda261dfd72444fe43
annebell881 <aeb178@tamu.edu>
2022-04-06 20:32:04 -0500

    checking an updated test

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "HomePages", type: :request do
   describe "able to access the index page from home" do
     it "get index page from nav bar" do
       get root_path
-      page.should have_link(href="/index")
+      root_path.should have_link(href="/index")
 
     end
   end
