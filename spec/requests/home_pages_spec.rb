5fe2b7a9b058afd97ca86dfb726a852e7dfa3afb
annebell881 <aeb178@tamu.edu>
2022-04-06 21:58:24 -0500

    checking an updated test

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe "HomePages", type: :request do
     it "get pages from nav bar" do
         get root_path
         page.should have_link(href="/index")
-        page.should have_link(href="/about")
-        page.should have_link(href="/recruitment")
       end
     end
   end
