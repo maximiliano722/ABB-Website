39116aeefaf436655c992f2b66a8884114209730
annebell881 <aeb178@tamu.edu>
2022-04-06 21:52:23 -0500

    checking an updated test

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "HomePages", type: :request do
   describe "able to access the about page from home" do
     it "get pages from nav bar" do
-      get root_path
+        get root_path
         page.should have_link(href="/index")
         page.should have_link(href="/about")
         page.should have_link(href="/recruitment")
       end
     end
   end
-end  
+ 
   
