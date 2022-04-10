1a375dcaa3442c282d386e8a58e91ccd02e4c761
khaihuyennguyen90 <khaihuyennguyen90@tamu.edu>
2022-04-10 14:34:51 -0500

    fix if log in as admin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,14 +6,18 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
   # Returns the current logged-in user (if any).
   # Instead going to database and checking all over again, create an instance variable
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id]) # this means "or equal" just like += or -=
-    elsif !@current_admin.nil?
-      email = current_admin.email
-      @current_user ||= User.find_by(email: email)
+    elsif !current_admin.nil?
+      #email_admin = current_admin.email
+      @current_user ||= User.find_by(email: current_admin.email)
     end
 
   end
