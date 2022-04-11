544ee0e076cef27c9caf5588d3e437453467c7b8
Maximiliano Zavala <m.zavala22@tamu.edu>
2022-04-10 23:07:29 -0500

    Added mailer

---
+++ spec/mailers/previews/question_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/question_mailer
+class QuestionMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/question_mailer/new_question
+  def new_question
+    QuestionMailer.new_question
+  end
+
+end
