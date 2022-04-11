c1fb440edd8c30227525ed27e39f5c35f7fd6299
annebell881 <aeb178@tamu.edu>
2022-04-10 20:23:46 -0500

    testing code coverage

--- spec/requests/home_page_flows_spec.rb
+++ spec/requests/home_page_flows_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "HomePageFlows", type: :request do
   describe "GET our teams page from index" do
     it "gets the our teams page from the index page" do
       get root_path
-      expect(page).to have_link('Our Team', href: root_path, count: 1)
+      expect(page).to have_link('Our Team', href= "/our_team" , count: 1)
     end
   end
 
@@ -12,7 +12,8 @@ RSpec.describe "HomePageFlows", type: :request do
     #Since form is not a entity(check for button)
     it "tests for a submit button on the home page" do
       get root_path
-      click_on "New Article"
+      click_on "Submit"
     end
   end
+
 end
