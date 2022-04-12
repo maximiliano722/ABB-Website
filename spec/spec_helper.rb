a1ebe120374c019321591adcb8fbc8fa30ed5f3e
khaihuyennguyen90 <khaihuyennguyen90@tamu.edu>
2022-04-11 22:43:51 -0500

    add integration test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.warnings = false
 end
--- spec/system/create_user_spec.rb
+++ spec/system/create_user_spec.rb
@@ -1,16 +1,38 @@
 require 'rails_helper'
 
-#Test 1: sign up new user
+#Test 1: sign up new user with invalid password
 
-RSpec.describe 'Creating a user', type: :system do
+RSpec.describe 'Creating a user', type: :feature do
     scenario 'valid inputs' do
         visit new_user_path
         fill_in 'Name', with: 'Khai Huyen Nguyen'
         fill_in 'Email', with: 'nguyenkhaihuyen1990@gmail.com'
-        fill_in 'Password', with: '1234567'
-        fill_in 'Confirm Password', with: '1234567'
         click_on 'Create Account'
-        visit_users_path
-        expect(page).to have_content('Khai Huyen Nguyen')
+        visit users_path
+        expect(page).to have_content('You need to sign in')
     end
+end
+#TEst 2: update a feature
+
+
+RSpec.describe('Authentication', type: :feature) do
+  before do
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:admin]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_user]
+    Admin.create!(email: 'khaihuyennguyen90@tamu.edu', full_name: 'Khai Huyen Nguyen', uid: '728002055', avatar_url: 'https://lh3.googleusent.com/url/photo.jpg')
+    User.create!(name:  "Hanbit",
+            email: "hk24536@tamu.edu",
+            password:              "1234567",
+            password_confirmation: "1234567",
+            is_admin: true)
+  end
+
+  it 'When a user try to see all users website should take them back to log in page' do
+    visit users_path
+    # verify redirection & the page content to be the sign in page
+    expect(page).to(have_current_path(new_admin_session_path))
+    expect(page).to(have_content('Login to the Admin Dashboard'))
+    expect(page).to(have_content('You need to sign in or sign up before continuing'))
+  end
+
 end
