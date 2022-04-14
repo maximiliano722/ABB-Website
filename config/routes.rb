12c47aefa948ee5c4073733f00b36fa319e15995
Olsi Sadiku <olsi_sadiku@hotmail.com>
2022-04-14 16:46:54 -0500

    Faq finishjed

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/index' => 'page#index'
+  get '/faq' => 'page#faq'
   post '/index' => 'membership#send_email'
   post '/sponsor' => 'membership#send_email'
   post '/recruitment' => 'membership#send_email'
