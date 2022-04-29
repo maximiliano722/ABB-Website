698e861bdcadcf388063d5ef52af8f4de574331c
maximiliano722 <43976078+maximiliano722@users.noreply.github.com>
2022-04-29 17:08:17 -0500

    Removed keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,16 +75,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '107609895920-ccfjscpj7en7ntcb6fv0n6rj71vvlr1v.apps.googleusercontent.com'
-  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'ABBcsce431@gmail.com',
-    password:             'quatnnsetdjlcivv',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true,
     open_timeout:         5,
